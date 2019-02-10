4d50c53e57ecda2fd3319503466904bd21a759eb
Steven Faulkner <sfaulkner1@gmail.com>
2019-02-10 16:16:30 -0500

    moved ui over to blessed

--- project1/index.js
+++ project1/index.js
@@ -1,12 +1,22 @@
 const WebSocket = require('ws');
 const blessed = require('blessed');
+const contrib = require('blessed-contrib')
+const cliCurosr = require('cli-cursor');
+
+cliCurosr.hide();
 
 screen = blessed.screen({
     smartCSR: true,
     dockBorders: true,
-    artificalCursor: true
+    autoPadding: true,
+    artificialCursor: true,
+    cursorShape: 'line',
+    cursorBlink: true,
+    cursorColor: null,
+    ignoreDockContrast: true
 })
 
+grid = new contrib.grid({rows: 5, cols: 5, screen: screen})
 
 host = process.argv[2] || 'localhost';
 port = process.argv[3] || '4930';
@@ -14,66 +24,72 @@ userName = process.argv[4] || 'username';
 
 const ws = new WebSocket(`ws://${host}:${port}/?username=${userName}`);
 
-const craftMessage = (from, to, kind, data) => JSON.stringify({
-    from, to, kind, data,
+
+
+screen.key(['escape', 'q', 'C-c'], (ch, key) => (process.exit(0)));
+
+screen.key('enter', (ch, key) => {
+  inputText.focus();
 });
 
 
-box = blessed.box({
-    top: 'top',
-    width: '100%',
-    height: '95%', 
-    keys: true,
-    mouse: true,
+box = grid.set(0, 0, 5, 5, contrib.log, {
+  label: 'Server Chat',
+  style:
+{ 
+  border: {
+  type: 'line'
+  },
+  bg: 'black',
+  fg: 'green'
+}})
+
+sidebar = grid.set(0,4,5,2, blessed.list, {
+  label: 'User List',
+  style:
+  {
     alwaysScroll: true,
-    scrollable: true,  
+    scrollable: true, 
+    mouse: true, 
+    list: [''],
     border: {
       type: 'line'
     },
-    style: {
-        bg: 'black',
-        fg: 'green'
-    }
-  });
+    bg: 'black',
+    fg: 'white'
+  }
+})
 
-inputText = blessed.textbox({
-      bottom: '0',
-      width: '100%',
-      height: '10%',
-      inputOnFocus: true,
-      border: {
-        type: 'line'
-      }
-  })
-  
-  // Append our box to the screen.
+inputText = grid.set(4,0,1,5, blessed.textbox, {
+  label: 'Message',
+  inputOnFocus: true,
+  keys: true,
+  mouse: true,
+  stle:
+  {
+    border: {
+      type: 'line'
+    }
+  }
+})
+
+
+
+  // Append elements box to the screen.
   screen.append(box);
   screen.append(inputText);
+  screen.append(sidebar);
 
-  screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-    return process.exit(0);
-  });
-  
-  // Focus our element.
   inputText.focus();
-  
-  // Render the screen.
   screen.render();
 
-ws.on('message', function incoming(message) {
-    
-    temp = JSON.parse(message);
-   
-    box.pushLine(temp.from + " : " + temp.data)
-    
-    screen.render();
 
-});
+
 
 
 inputText.on('submit', (line) => {
     
-    if(line.trim() != 'null')
+    if(line.trim() != '')
     {
         ws.send(JSON.stringify({
             from: userName,
@@ -84,18 +100,67 @@ inputText.on('submit', (line) => {
     }
     inputText.clearValue();
     inputText.focus();
-  });
+});
+
+ws.on('message', function incoming(message) {
+    
+    let temp = JSON.parse(message);
+    switch(temp.kind){
+    
+    case 'chat':       box.log(temp.from + " : " + temp.data);
+                        break;
+    case 'connection': box.log(temp.from + " : " + temp.data);
+                        addRemoveUser(temp.data);
+                        break;
+    case 'userlist':   sidebarHandler(temp.data);
+                        break;
+    case 'error':      box.log(temp.from + " : " + temp.data);
+                        break;
+    }
+    screen.render();
+
+});
 
 
-  screen.key('enter', (ch, key) => {
-    inputText.focus();
-  });
+function sidebarHandler(data){
+    let users = data.split(',');
 
-  screen.key(['escape', 'q', 'C-c'], (ch, key) => (process.exit(0)));
+    sidebar.clearItems();
+
+    users.forEach(element => {
+      if(element == userName){
+      sidebar.pushItem(element + ' (you)');
+      }else{
+        sidebar.pushItem(element);
+      }
+    });
+}
 
 
+function addRemoveUser(data){
+  
+  let user = data.split(' ');
 
+  if(user[2] == 'joined'){
+    if(user[0] == userName){
+      sidebar.pushItem(user[0]+'(you)');
+    }else{
+      sidebar.pushItem(user[0]);
+    }
+  }else{
+    sidebar.removeItem(user[0]);
+  }
+  
+  
+}
 
-
-
-
+setInterval(() =>{
+  ws.send(JSON.stringify({
+    from: userName,
+    to: '',
+    kind: 'userlist',
+    data: ''
+  }))
+  inputText.focus();
+ 
+}, 1000)
