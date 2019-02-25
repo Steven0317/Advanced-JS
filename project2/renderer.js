c37f81749bfdeb81f15d7a577f5ad7ceb9ec050c
Steven Faulkner <sfaulkner1@gmail.com>
2019-02-25 16:20:36 -0500

    figured out the layout

--- project2/renderer.js
+++ project2/renderer.js
@@ -5,19 +5,25 @@ init();
 function init() {
 
     
-        WIDTH  = window.innerWidth ,
+        WIDTH  = 640,
         HEIGHT = 600;
-        drawingSurface = document.getElementById('treeview');
-        renderer = new THREE.WebGLRenderer({antialias: true, canvas: drawingSurface});
-        renderer.setSize( WIDTH,HEIGHT );
-        document.body.appendChild( renderer.domElement );
-            
-        scene = new THREE.Scene();
+        container = document.getElementById('treeview');
+        renderer = new THREE.WebGLRenderer({antialias: true});
+        //renderer.setPixelRatio(window.devicePixelRatio);
+        
+        
+        w = container.offsetWidth;
+        h = container.offsetHeight;
+        container.appendChild( renderer.domElement );
+        renderer.setSize(w,h);
+
+        
+        scene = new THREE.Scene(w,h);
             
         camera = new THREE.PerspectiveCamera(
             50,             // Field of view
-            WIDTH/HEIGHT,   // Aspect ratio
-            .01,              // Near plane
+            w/h,            // Aspect ratio
+            .01,            // Near plane
             20000           // Far plane
         );
         camera.position.set(2,10,12);
@@ -28,10 +34,10 @@ function init() {
 
         // Create an event listener that resizes the renderer with the browser window.
         window.addEventListener('resize', function() {
-            WIDTH = window.innerWidth ,
-            HEIGHT = window.innerHeight;
-            renderer.setSize(WIDTH, HEIGHT);
-            camera.aspect = WIDTH / HEIGHT;
+          w = container.offsetWidth;
+          h = container.offsetHeight;
+            renderer.setSize(w,h);
+            camera.aspect = w/h;
             camera.updateProjectionMatrix();
         });
     
@@ -40,7 +46,7 @@ function init() {
         light.position.set( -100, 300, 200 );
         scene.add( light );
         
-        
+        //box creation
         geometry = new THREE.BoxGeometry( 8, 1, 8 );
         material = new THREE.MeshLambertMaterial( { color: 'lightgray' } );
         mesh = new THREE.Mesh( geometry, material );
