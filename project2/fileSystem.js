20efbb527cb95d059b05558b2e711df873632cfb
Steven Faulkner <sfaulkner1@gmail.com>
2019-04-08 21:57:46 -0400

    fixed file directory to start at drive level

--- project2/fileSystem.js
+++ project2/fileSystem.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const dirTree = require("directory-tree");
 const getSize = require('get-folder-size');
-const homedir = require('os').homedir();
+const hide = require('hidefile');
 
 init();
 
@@ -10,16 +9,23 @@ init();
 function init(){
     
     ul = document.getElementById("directory");
-    fs.readdir(homedir,(err, file) => {
+   
+    fs.readdir('/',(err, file) => {
+        
         file.forEach(file => {
-            if(file[0] !== "." && file[6] !== "."){
-                li = document.createElement("li");
-                link = document.createElement("button");
-                link.innerHTML = file.toString();
-                //link.href = "http://www.google.com";
-                li.appendChild(link);
-                ul.appendChild(li); 
-            }
+           
+            hide.shouldBeHidden('/' + file, (err, result) => {
+               
+                if(err !== null) throw err;
+                if(!result) {
+                    li = document.createElement("li");
+                    link = document.createElement("button");
+                    link.innerHTML = file.toString();
+                    //link.href = "http://www.google.com";
+                    li.appendChild(link);
+                    ul.appendChild(li); 
+                }
+            })
         })
     })
 
