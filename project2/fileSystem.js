1c858ffc271c509e90cf4d772be98f6fbb0471f8
Steven Faulkner <sfaulkner1@gmail.com>
2019-04-18 01:12:28 -0400

    fixes for things

--- project2/fileSystem.js
+++ project2/fileSystem.js
@@ -7,101 +7,20 @@ homedir = '/'
 
 //init();
 Init();
+
+
 function Init(){
     
     renderer.Init();
     fs.readdir(homedir,(err,file) =>{
-        let i = 0;
-        let j = 0;
         if(err !== null) throw err;
 
-        file.forEach(file => {
-
-            hide.shouldBeHidden(homedir + file, (err, result) => {
-               
-                if(err !== null) throw err;
-                
-                if(!result) {
-                    if(fs.statSync(path.join(homedir,file)).isDirectory()) {
-                         renderer.createObject(
-                                                file,
-                                                path.join(homedir,file,),
-                                                'dir',
-                                                i
-                                                )
-                         }
-                         if(i >=5){
-                             i = 0;
-                         }else {
-                         i++;
-                         }
-                        j++;
-
-                }
-
-            })
-
-        })
-
+        renderer.createObjects(file, homedir);
     })
 }
 
-function clickEvent(btn,text,path){
-    console.log(text);
-}
-
-function init(){
-   
-    //leaving this global allows us to clear it in
-    // utility functions
-    display_grid = document.getElementById('grid');
-
-    fs.readdir(homedir,(err, file) => {
-        
-        if(err !== null) throw err;
-
-        file.forEach(file => {
-           
-            hide.shouldBeHidden(homedir + file, (err, result) => {
-               
-                if(err !== null) throw err;
-                
-                if(!result) {
-               
-                    let li = document.createElement("div");
-                    li.className = ".grid-item";
-                    let link = document.createElement("button");
-                    let elem = document.createElement("img");
-
-                   
-                    if(fs.statSync(path.join(homedir,file)).isDirectory()) {
-                        elem.setAttribute("src", "Images/Folder-128.png");
-                        link.value = path.join(homedir,file.toString(),'/');
-                        let sp = document.createElement('span');
-                        sp.textContent = file.toString();
-                        link.appendChild(elem);
-                        link.appendChild(sp);
-                        
-                    }else {
-                        elem.setAttribute("src", "Images/Automater-128.png");
-                        link.value = path.join(homedir,file.toString(),'/');
-                        let sp = document.createElement('span');
-                        sp.textContent = file.toString();
-                        link.appendChild(elem);
-                        link.appendChild(sp);
-                        
-                    }
-                    link.addEventListener('click', () => {
-                        clicked(link);
-                    })
-                    li.appendChild(link);
-                    display_grid.appendChild(li); 
-                }
-            })
-        })
-       
-    })
-
+function clickEvent(param, e){
+    objectClick(param);
 }
 
 /*
@@ -109,87 +28,22 @@ function init(){
 *   checks if event is linked to a directory
 *   will erase and create new display if so
 */
-function clicked(clicked_object){
-   
-    if(fs.statSync(path.join(clicked_object.value)).isDirectory()){
-        
-         // clear old display grid //
-        while(display_grid.firstChild) {
-            display_grid.removeChild(display_grid.firstChild);
-        }
+function objectClick(clicked_path){
 
-        homedir = clicked_object.value + '/';
-        
-        fs.readdir(homedir,(err, file) => {
-            
+    //check if clicked objec is a directory
+    if(fs.statSync(path.join(clicked_path)).isDirectory()){
+
+        renderer.clearScreen();
+
+        homedir = path.join(clicked_path,'/');
+       
+        fs.readdir(homedir,(err,file) =>{
             if(err !== null) throw err;
-
-            file.forEach(file => {
-            
-                hide.shouldBeHidden(homedir + file, (err, result) => {
-                
-                    if(err !== null) throw err;
-                    
-                    if(!result) {
-                    
-                        let li = document.createElement("div");
-                        li.className = ".grid-item";
-                        let link = document.createElement("button");
-                        let elem = document.createElement("img");
-
-                        if(fs.statSync(path.join(homedir,file)).isDirectory()) {
-                           
-                            elem.setAttribute("src", "Images/Folder-128.png");
-                            
-                            link.value = path.join(homedir,file.toString(),'/');
-                            let sp = document.createElement('span');
-                            sp.textContent = file.toString();
-                            link.appendChild(elem);
-                            link.appendChild(sp);
-                            
-                        }else {
-                            if(file.toString().match(/exe/)){
-                                elem.setAttribute("src", "Images/Automator-128.png");
-                            }
-                            else if(file.toString().match(/jpg|jpeg|png/)){
-                                elem.setAttribute("src", "Images/iPhoto-128.png");
-                            }
-                            else if(file.toString().match(/mov|mp4|wmv|avi/)){
-                                elem.setAttribute("src", "Images/iMovie-128.png");
-                            }
-                            else if(file.toString().match(/pdf/)){
-                                elem.setAttribute("src", "Images/Text-edit-128.png");
-                            }
-                            else if(file.toString().match(/txt/)){
-                                elem.setAttribute("src", "Images/Text-edit-128.png");
-                            }
-                            else{
-                                elem.setAttribute("src", "Images/File Blank-128.png");
-                            }
-                            
-                            link.value = path.join(homedir,file.toString(),'/');
-                            let sp = document.createElement('span');
-                            sp.textContent = file.toString();
-                            link.appendChild(elem);
-                            link.appendChild(sp);
-                            
-                        }
-                        link.addEventListener('click', () => {
-                            clicked(link);
-                        })
-                        li.appendChild(link);
-                        display_grid.appendChild(li); 
-
-                    }
-
-
-                })
-
-            })
-
+           
+            renderer.createObjects(file, homedir);
         })
+
     }
 }
-
 
 module.exports. clickEvent = clickEvent;
--- project2/renderer.js
+++ project2/renderer.js
@@ -1,23 +1,26 @@
 const PIXI = require('pixi.js');
 const system = require('./fileSystem.js');
-
+const fs = require('fs');
+const hide = require('hidefile');
+const path = require('path');
 
 /*  
 *   each object will be given a color from this
 *   dictionary
 */
 colorDict = {
-    'blue':   0x89CEF0,
-    'orange': 0xFFA48C,
-    'yellow': 0xFFDF8C,
-    'purple': 0x939FF2
+    'dir':   0x89CEF0,
+    'file': 0xFFA48C,
+    'pic': 0xFFDF8C,
+    'bin': 0x939FF2,
+    'else': 0x86F3B5
 };
 /*
 * text style for all rendered text
 */
 const style = new PIXI.TextStyle({
     fontFamily: 'Monospace',
-    fontSize: 18,
+    fontSize: 16,
     wordWrap: true,
     wordWrapWidth: 100
 })
@@ -28,7 +31,7 @@ function Init(){
 
     let elem = document.getElementById('treeview');
 
-    const app = new PIXI.Application({
+    app = new PIXI.Application({
         width: 850,
         height: 600,
         backgroundColor: 0xeeeeee,
@@ -45,55 +48,133 @@ function Init(){
     container.x = 0;
     container.y = 0;
 
-    container.pivot.x = 0;
+    container.pivot.x =0;
     container.pivot.y = 0;
+
 }
 
-function createObject(name, path, type, i){
+function createObjects(files, homedir){
     /*
     *   create a rectangle object for each file
     *   params that are passed 
     */
-
-    let rect = new PIXI.Graphics();
-    if(type == "dir"){
-
+    if(files.length > 5){
+        rectWidth = Math.floor(app.view.width / 5);
+    }else {
+        rectWidth = Math.floor(app.view.width / files.length);
+    }
+    rectHeight = Math.floor(app.view.height / Math.round(files.length/5));
+    i = 0;
+    j = 0;
+    files.forEach(file => {
+     
         /*
         * create rectangle object
         */
-        rect.beginFill(colorDict['blue'],1);
-        rect.drawRect((i % 4) * 205, Math.floor((i / 4)) * 205, 200,200);
-        rect.endFill();
-        rect.interactive = true;
-        rect.buttonMode = true;
-        
-       
 
-        /*
-        *   create text object
-        */
-       let text = new PIXI.Text(name, style);
-       console.log(rect.x)
-       text.x = ((i%4) * 205) + (125/2);
-       text.y = (Math.floor((i / 4)) * 205) + (150/2);
-      
-       /*
-       *    button bindings
-       */
-       rect.on('pointerover', onButtonOver(rect));
-       rect.click = system.clickEvent.bind(rect, text, path);
-       
-       /*
-       *append created object to container
-       */
-       container.addChild(rect);
-       container.addChild(text);
+        hide.shouldBeHidden( path.join(homedir,file),(err,result) => {
+            
+            if(err !== null) throw err;
+
+            if(!result){
+
+                let rect = new PIXI.Graphics();
+                
+                if(fs.statSync(path.join(homedir,file)).isDirectory()) {
+                    rect.beginFill(colorDict['dir'],1);
+                    rect.drawRect((i) * rectWidth, j * rectHeight, rectWidth,rectHeight);
+                    rect.endFill();
+                }
+                else{
+
+                    if(file.toString().match(/exe/)){
+                        rect.beginFill(colorDict['bin'],1)
+                        rect.drawRect((i) * rectWidth, j * rectHeight, rectWidth,rectHeight);
+                        rect.endFill();
+                    }
+                    else if(file.toString().match(/jpg|jpeg|png|gif/)){
+                        rect.beginFill(colorDict['pic'],1)
+                        rect.drawRect((i) * rectWidth, j * rectHeight, rectWidth,rectHeight);
+                        rect.endFill();
+                    }
+                    else if(file.toString().match(/mov|mp4|wmv|avi/)){
+                        rect.beginFill(colorDict['file'],1);
+                        rect.drawRect((i) * rectWidth, j * rectHeight, rectWidth,rectHeight);
+                        rect.endFill();
+                    }
+                    else if(file.toString().match(/pdf/)){
+                        rect.beginFill(colorDict['file'],1);
+                        rect.drawRect((i) * rectWidth, j * rectHeight, rectWidth,rectHeight);
+                        rect.endFill();
+                    }
+                    else if(file.toString().match(/txt/)){
+                        rect.beginFill(colorDict['file'],1);
+                        rect.drawRect((i) * rectWidth, j * rectHeight, rectWidth,rectHeight);
+                        rect.endFill();
+                    }
+                    else{
+                        rect.beginFill(colorDict['else'],1);
+                        rect.drawRect((i) * rectWidth, j * rectHeight, rectWidth,rectHeight);
+                        rect.endFill();
+                    }
+                
+                    
+                }
+                rect.interactive = true;
+                rect.buttonMode = true;
+
+                /*
+                *   create text object
+                */
+                if(file.length > 20){
+                    str = file.substring(0,10);
+                }
+                else{
+                    str = file;
+                }
+                let text = new PIXI.Text(str, style);
+                text.anchor.set(0.5,0.5);
+                
+                text.x = ((i) * rectWidth) + (rectWidth/2);
+                text.y = (j * rectHeight) + (rectHeight/2);
+                
+                /*
+                *    button bindings
+                */
+                rect.mouseover = function(mouseData){
+                    this.alpha = .5;
+                }
+                rect.mouseout = function(mouseData){
+                    this.alpha = 1;
+                }
+                rect.on('click', system.clickEvent.bind(undefined, path.join(homedir,file)))
+                
+                /*
+                *append created object to container
+                */
+                container.addChild(rect);
+                container.addChild(text);
+            
+                if(i >5){
+                    i = 0;
+                    j += 1;
+                }else {
+                    i += 1;
+                }
+            }
+         });
+    });
+   
+    
+}
+
+function clearScreen(){
+    while(container.children.length > 0){
+        let child = container.getChildAt(0);
+        container.removeChild(child);
     }
 }
 
-function onButtonOver(btn){
-    console.log(btn);
-}
-
+module.exports.clearScreen = clearScreen;
 module.exports.Init = Init;
-module.exports.createObject = createObject;
+module.exports.createObjects = createObjects;
